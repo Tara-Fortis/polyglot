00a8a9eccfe466015652ee8999cc5993ee95df24
Tineil_A <tine6573@gmail.com>
2026-04-13 16:03:48 -0400

    Final project

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Nav } from './components/nav/nav';
 
 // transloco import
-import { TranslocoModule, TranslocoDirective } from '@jsverse/transloco';
+import { TranslocoModule} from '@jsverse/transloco';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TranslocoModule, TranslocoDirective, Nav],
+  imports: [RouterOutlet, TranslocoModule, Nav],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
